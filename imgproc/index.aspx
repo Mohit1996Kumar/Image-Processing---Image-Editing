67b4db453b16b005b45908a80d80d466d17d0fd7
Mohit Kumar <43097997+Mohit1996Kumar@users.noreply.github.com>
2018-09-10 00:27:56 +0530

    Uploaded a new project
    Image Editing online

---
+++ imgproc/index.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="imgproc.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #myimg {
+            height: 17px;
+            width: 11px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+          <asp:FileUpload id="FileUploadControl" runat="server" />
+        <asp:Button runat="server" id="UploadButton" text="Upload" onclick="UploadButton_Click" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    
+        <asp:Image ID="Image1" runat="server" />
+        <br />
+          <br />
+        <br />
+    
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <asp:CheckBox ID="CheckBox1" runat="server" Text="Grayscale" />
+          <asp:CheckBox ID="CheckBox2" runat="server" Text="Resize" />
+        <br />
+        <br />
+        <img id="myimg" alt="" src="" runat="server"/>
+          <br />
+          <br />
+          <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Apply Filter" Width="112px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <asp:Button ID="Button2" runat="server" Text="Download Image" Width="119px" />
+          <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ imgproc/index.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SoundInTheory.DynamicImage;
+using SoundInTheory.DynamicImage.Caching;
+using SoundInTheory.DynamicImage.Layers;
+using SoundInTheory.DynamicImage.Filters;
+using SoundInTheory.DynamicImage.Sources;
+using SoundInTheory.DynamicImage.Configuration;
+using SoundInTheory.DynamicImage.Util;
+using SoundInTheory.DynamicImage.Fluent;
+using System.IO;
+
+namespace imgproc
+{
+    public partial class index : System.Web.UI.Page
+    {
+        string filename;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            
+            
+        }
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            if (FileUploadControl.HasFile)
+            {
+                try
+                {
+                    filename = Path.GetFileName(FileUploadControl.FileName);
+                    FileUploadControl.SaveAs(Server.MapPath("~/" + filename));
+                    Image1.ImageUrl = "~/" + filename;
+                }
+                catch (Exception ex)
+                {
+                    Label1.Text = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Composition composition = new Composition();
+            var myLayer = new ImageLayer();
+            myLayer.SourceFileName = Image1.ImageUrl;
+            composition.Layers.Add(myLayer);
+            myLayer.Filters.Add(new ColorTintFilter());
+            myLayer.Filters.Add(new ResizeFilter { Mode = ResizeMode.UseWidth, Width = SoundInTheory.DynamicImage.Unit.Pixel(1200) });
+            myLayer.Filters.Add(new RotationFilter { Angle = 30 });
+            string url = ImageUrlGenerator.GetImageUrl(composition);
+            myimg.Attributes["src"] = url;
+            myimg.Attributes["style"] = "width:500px; height:500px;";
+            //   myimg.Attributes["width"] = "200";
+         //   myimg.Attributes["height"] = "200";
+
+        }
+    }
+}
